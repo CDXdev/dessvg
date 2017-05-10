a641fac2cd699b3ccef7cfefb234aca54be6870d
Guillaume Grésillion <guillaume.gresillion@gmail.com>
2017-05-10 22:28:36 +0200

    Bug fix on images

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { DrawAreaComponent } from './draw-area/draw-area.component';
 
